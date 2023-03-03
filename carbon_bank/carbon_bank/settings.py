c5d4a6fc50b99a9b56ea68bba54be94bdf89ae45
selcukakarin <selcukakarin@gmail.com>
2023-03-03 10:19:13 +0300

    comment out db postgres

--- carbon_bank/carbon_bank/settings.py
+++ carbon_bank/carbon_bank/settings.py
@@ -97,23 +97,23 @@ WSGI_APPLICATION = 'carbon_bank.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 #
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'databasepostgresql',
-#         'USER': 'databasepostgresql_user',
-#         'PASSWORD': 'databasepostgresql_password',
-#         'HOST': 'databasepostgresql',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'databasepostgresql',
+        'USER': 'databasepostgresql_user',
+        'PASSWORD': 'databasepostgresql_password',
+        'HOST': 'databasepostgresql',
+        'PORT': '5432',
     }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 
 # Password validation
